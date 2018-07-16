bd8d8e9cd08738b37083bc73c1032da39f53a958
Omar Kallel <omar.kallel@pivasoftware.com>
2018-07-16 14:04:10 +0100

    Copy right headers

--- stun/config.c
+++ stun/config.c
@@ -1,9 +1,23 @@
 /*
  * config.c -- contains functions that allows reading and loading of uci config parameters of stun
  *
- * TR-069 STUN  client software
- *	Copyright (C) 2018 PIVA SOFTWARE <www.pivasoftware.com> - All Rights Reserved
- *		Author: Omar Kallel <omar.kallel@pivasoftware.com>
+ *  Copyright (C) 2018 Inteno Broadband Technology AB. All rights reserved.
+ *
+ *  Author: Omar Kallel <omar.kallel@pivasoftware.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  version 2 as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *  02110-1301 USA
  */
 
 #include "config.h"
