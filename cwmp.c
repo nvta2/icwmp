b6d8e31ddca4fe22efaf252fe3f582d6fa80cbb7
Omar Kallel <omar.kallel@pivasoftware.com>
2020-06-10 10:57:18 +0100

    Fix session cookies issue

--- cwmp.c
+++ cwmp.c
@@ -175,7 +175,9 @@ void cwmp_schedule_session (struct cwmp *cwmp)
         }
 
         session = list_entry(ilist, struct session, list);
-
+        cwmp_add_notification_min();
+    	if (access(fc_cookies, F_OK) != -1)
+    		remove(fc_cookies);
         cwmp_prepare_value_change(cwmp, session);
 
         if (error = cwmp_move_session_to_session_send (cwmp, session))
@@ -187,7 +189,8 @@ void cwmp_schedule_session (struct cwmp *cwmp)
         cwmp->session_status.last_start_time = time(NULL);
         cwmp->session_status.last_status = SESSION_RUNNING;
         cwmp->session_status.next_retry = 0;
-        cwmp_add_notification_min();
+
+
         CWMP_LOG (INFO,"Start session");
         uci_get_value(UCI_CPE_EXEC_DOWNLOAD, &exec_download);
         if(strcmp(exec_download, "1") == 0){
