1f43e2f051d7dc12bf1da37670c072ab58d099d5
Omar Kallel <omar.kallel@pivasoftware.com>
2021-10-13 16:44:44 +0100

    Update year in head comments of test files

--- test/cmocka/icwmp_datamodel_interface_unit_test.c
+++ test/cmocka/icwmp_datamodel_interface_unit_test.c
@@ -1,3 +1,13 @@
+/*
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Copyright (C) 2013-2021 iopsys Software Solutions AB
+ *	  Author Omar Kallel <omar.kallel@pivasoftware.com>
+ */
+
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
--- test/cmocka/icwmp_soap_msg_unit_test.c
+++ test/cmocka/icwmp_soap_msg_unit_test.c
@@ -4,7 +4,7 @@
  *	the Free Software Foundation, either version 2 of the License, or
  *	(at your option) any later version.
  *
- *	Copyright (C) 2013-2020 iopsys Software Solutions AB
+ *	Copyright (C) 2013-2021 iopsys Software Solutions AB
  *	  Author Omar Kallel <omar.kallel@pivasoftware.com>
  */
 
