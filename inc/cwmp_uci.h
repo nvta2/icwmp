b1d018afd210b82de7965cda0431ee57471a6cc4
Omar Kallel <omar.kallel@pivasoftware.com>
2022-03-02 15:15:13 +0100

    Ticket refs #7512: icwmpd: Connect imediatly on wan up

--- inc/cwmp_uci.h
+++ inc/cwmp_uci.h
@@ -33,7 +33,6 @@
 #define UCI_LOG_SEVERITY_PATH "cwmp.cpe.log_severity"
 #define UCI_CPE_USERID_PATH "cwmp.cpe.userid"
 #define UCI_CPE_PASSWD_PATH "cwmp.cpe.passwd"
-#define UCI_CPE_INTERFACE_PATH "cwmp.cpe.interface"
 #define UCI_CPE_UBUS_SOCKET_PATH "cwmp.cpe.ubus_socket"
 #define UCI_CPE_PORT_PATH "cwmp.cpe.port"
 #define UCI_CPE_DEFAULT_WAN_IFACE "cwmp.cpe.default_wan_interface"
